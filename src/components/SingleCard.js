3d7118f61661b3767717ce61d0500ae1993286c8
ferzxk <mmiernik47@gmail.com>
2024-11-25 21:46:01 +0100

    update img paths

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -11,9 +11,9 @@ const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
   return (
     <div className='card'>
       <div className={flipped ? 'flipped' : ''}>
-        <img className='front' src={card.src} alt='card front' />
-        <img className='back' src='/img/cover.png' alt='card back' onClick={handleClick} />
-      </div>
+      <img className='front' src={card.src.startsWith('/img') ? `${process.env.PUBLIC_URL}${card.src}` : card.src} alt='card front' />
+      <img className='back' src={`${process.env.PUBLIC_URL}/img/cover.png`} alt='card back' onClick={handleClick} />
+        </div>
     </div>
   )
 }
